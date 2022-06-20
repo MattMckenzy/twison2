38ffdbf649c31ece39bd794f0dd53b9ec5d12456
MattMckenzy <mattmckenzy@hotmail.com>
2022-06-20 12:32:23 -0400

    Added image extraction capabilitites to Twison.

--- src/twison.js
+++ src/twison.js
@@ -1,187 +1,222 @@
 var Twison = {
 
-  /**
-   * Extract the link entities from the provided text.
-   *
-   * Text containing [[foo]] would yield a link named "foo" pointing to the
-   * "foo" passage.
-   * Text containing [[foo->bar]] would yield a link named "foo" pointing to the
-   * "bar" passage.
-   *
-   * @param {String} text
-   *   The text to examine.
-   *
-   * @return {Array|null}
-   *   The array of link objects, containing a `name` and a `link`.
-   */
-  extractLinksFromText: function(text) {
-    var links = text.match(/\[\[.+?\]\]/g);
-    if (!links) {
-      return null;
-    }
-
-    return links.map(function(link) {
-      var differentName = link.match(/\[\[(.*?)\-\&gt;(.*?)\]\]/);
-      if (differentName) {
-        // [[name->link]]
-        return {
-          name: differentName[1],
-          link: differentName[2]
-        };
-      } else {
-        // [[link]]
-        link = link.substring(2, link.length-2)
-        return {
-          name: link,
-          link: link
+    /**
+     * Extract the link entities from the provided text.
+     *
+     * Text containing [[foo]] would yield a link named "foo" pointing to the
+     * "foo" passage.
+     * Text containing [[foo->bar]] would yield a link named "foo" pointing to the
+     * "bar" passage.
+     *
+     * @param {String} text
+     *   The text to examine.
+     *
+     * @return {Array|null}
+     *   The array of link objects, containing a `name` and a `link`.
+     */
+    extractLinksFromText: function (text) {
+        var links = text.match(/\[\[.+?\]\]/g);
+        if (!links) {
+            return null;
         }
-      }
-    });
-  },
 
-  /**
-   * Extract the prop entities from the provided text.
-   *
-   * A provided {{foo}}bar{{/foo}} prop would yield an object of `{"foo": 'bar'}`.
-   * Nested props are supported by nesting multiple {{prop}}s within one
-   * another.
-   *
-   * @param {String} text
-   *   The text to examine.
-   *
-   * @return {Object|null}
-   *   An object containing all of the props found.
-   */
-  extractPropsFromText: function(text) {
-    var props = {};
-    var propMatch;
-    var matchFound = false;
-    const propRegexPattern = /\{\{((\s|\S)+?)\}\}((\s|\S)+?)\{\{\/\1\}\}/gm;
+        return links.map(function (link) {
+            var differentName = link.match(/\[\[(.*?)\-\&gt;(.*?)\]\]/);
+            if (differentName) {
+                // [[name->link]]
+                return {
+                    name: differentName[1],
+                    link: differentName[2]
+                };
+            } else {
+                // [[link]]
+                link = link.substring(2, link.length - 2)
+                return {
+                    name: link,
+                    link: link
+                }
+            }
+        });
+    },
 
-    while ((propMatch = propRegexPattern.exec(text)) !== null) {
-      // The "key" of the prop, AKA the value wrapped in {{ }}.
-      const key = propMatch[1];
+    /**
+     * Extract the prop entities from the provided text.
+     *
+     * A provided {{foo}}bar{{/foo}} prop would yield an object of `{"foo": 'bar'}`.
+     * Nested props are supported by nesting multiple {{prop}}s within one
+     * another.
+     *
+     * @param {String} text
+     *   The text to examine.
+     *
+     * @return {Object|null}
+     *   An object containing all of the props found.
+     */
+    extractPropsFromText: function (text) {
+        var props = {};
+        var propMatch;
+        var matchFound = false;
+        const propRegexPattern = /\{\{((\s|\S)+?)\}\}((\s|\S)+?)\{\{\/\1\}\}/gm;
 
-      // Extract and sanitize the actual value.
-      // This will remove any new lines.
-      const value = propMatch[3].replace(/(\r\n|\n|\r)/gm, '');
+        while ((propMatch = propRegexPattern.exec(text)) !== null) {
+            // The "key" of the prop, AKA the value wrapped in {{ }}.
+            const key = propMatch[1];
 
-      // We can nest props like so: {{foo}}{{bar}}value{{/bar}}{{/foo}},
-      // so call this same method again to extract the values further.
-      const furtherExtraction = this.extractPropsFromText(value);
+            // Extract and sanitize the actual value.
+            // This will remove any new lines.
+            const value = propMatch[3].replace(/(\r\n|\n|\r)/gm, '');
 
-      if (furtherExtraction !== null) {
-        props[key] = furtherExtraction;
-      } else {
-        props[key] = value;
-      }
+            // We can nest props like so: {{foo}}{{bar}}value{{/bar}}{{/foo}},
+            // so call this same method again to extract the values further.
+            const furtherExtraction = this.extractPropsFromText(value);
 
-      matchFound = true;
-    }
+            if (furtherExtraction !== null) {
+                props[key] = furtherExtraction;
+            } else {
+                props[key] = value;
+            }
 
-    if (!matchFound) {
-      return null;
-    }
+            matchFound = true;
+        }
 
-    return props;
-  },
+        if (!matchFound) {
+            return null;
+        }
 
-  /**
-   * Convert an entire passage.
-   *
-   * @param {Object} passage
-   *   The passage data HTML element.
-   *
-   * @return {Object}
-   *   Object containing specific passage data. Examples include `name`, `pid`,
-   *   `position`, etc.
-   */
-  convertPassage: function(passage) {
-  	var dict = {text: passage.innerHTML};
+        return props;
+    },
 
-    var links = Twison.extractLinksFromText(dict.text);
-    if (links) {
-      dict.links = links;
-    }
+    /**
+     * Extract any referenced images from the provided text.
+     *
+     * Text containing <img src="url"> would yield an array of images with "url".
+     *
+     * @param {String} text
+     *   The text to examine.
+     *
+     * @return {Array|null}
+     *   The array of image urls.
+     */
+    extractImagesFromText: function (passage, text, tags) {
 
-    const props = Twison.extractPropsFromText(dict.text);
-    if (props) {
-      dict.props = props;
-    }
+        var images = [];
 
-    ["name", "pid", "position", "tags"].forEach(function(attr) {
-      var value = passage.attributes[attr].value;
-      if (value) {
-        dict[attr] = value;
-      }
-    });
+        var matches, expression = new RegExp('((?:https|file)?:\/\/.*?\.(?:png|jpg|webp|avif|gif|jpeg|svg))','igs');
+        while ((matches = expression.exec(text)) !== null) {
+            images.push(matches[1]);
+        }
 
-    if(dict.position) {
-      var position = dict.position.split(',')
-      dict.position = {
-        x: position[0],
-        y: position[1]
-      }
-    }
+        var selectors = tags.replace(' ', '|');
+        var style = document.getElementById('twine-user-stylesheet').innerText;
+        expression = new RegExp('"(?:' + selectors + ')".*?{.*?((?:https|file)?:\/\/.*?\.(?:png|jpg|webp|avif|gif|jpeg|svg)).*?}', 'igs');
+        while ((matches = expression.exec(style)) !== null) {
+            images.push(matches[1]);
+        }
+        
+	    return images;
+    },
 
-    if (dict.tags) {
-      dict.tags = dict.tags.split(" ");
-    }
+    /**
+     * Convert an entire passage.
+     *
+     * @param {Object} passage
+     *   The passage data HTML element.
+     *
+     * @return {Object}
+     *   Object containing specific passage data. Examples include `name`, `pid`,
+     *   `position`, etc.
+     */
+    convertPassage: function(passage) {
+        var dict = { text: passage.innerHTML };
 
-    return dict;
+        var links = Twison.extractLinksFromText(dict.text);
+        if (links) {
+            dict.links = links;
+        }
+
+        const props = Twison.extractPropsFromText(dict.text);
+        if (props) {
+            dict.props = props;
+        }
+
+        const images = Twison.extractImagesFromText(passage, dict.text, passage.attributes["tags"].value);
+        if (images) {
+            dict.images = images;
+        }
+
+        ["name", "pid", "position", "tags"].forEach(function (attr) {
+            var value = passage.attributes[attr].value;
+            if (value) {
+                dict[attr] = value;
+            }
+        });
+
+        if (dict.position) {
+            var position = dict.position.split(',')
+            dict.position = {
+                x: position[0],
+                y: position[1]
+            }
+        }
+
+        if (dict.tags) {
+            dict.tags = dict.tags.split(" ");
+        }
+
+        return dict;
 	},
 
-  /**
-   * Convert an entire story.
-   *
-   * @param {Object} story
-   *   The story data HTML element.
-   *
-   * @return {Object}
-   *   Object containing processed "passages" of data.
-   */
-  convertStory: function(story) {
+/**
+ * Convert an entire story.
+ *
+ * @param {Object} story
+ *   The story data HTML element.
+ *
+ * @return {Object}
+ *   Object containing processed "passages" of data.
+ */
+convertStory: function(story) {
     var passages = story.getElementsByTagName("tw-passagedata");
     var convertedPassages = Array.prototype.slice.call(passages).map(Twison.convertPassage);
 
     var dict = {
-      passages: convertedPassages
+        passages: convertedPassages
     };
 
-    ["name", "startnode", "creator", "creator-version", "ifid"].forEach(function(attr) {
-      var value = story.attributes[attr].value;
-      if (value) {
-        dict[attr] = value;
-      }
+    ["name", "startnode", "creator", "creator-version", "ifid"].forEach(function (attr) {
+        var value = story.attributes[attr].value;
+        if (value) {
+            dict[attr] = value;
+        }
     });
 
     // Add PIDs to links
     var pidsByName = {};
-    dict.passages.forEach(function(passage) {
-      pidsByName[passage.name] = passage.pid;
+    dict.passages.forEach(function (passage) {
+        pidsByName[passage.name] = passage.pid;
     });
 
-    dict.passages.forEach(function(passage) {
-      if (!passage.links) return;
-      passage.links.forEach(function(link) {
-        link.pid = pidsByName[link.link];
-        if (!link.pid) {
-          link.broken = true;
-        }
-      });
+    dict.passages.forEach(function (passage) {
+        if (!passage.links) return;
+        passage.links.forEach(function (link) {
+            link.pid = pidsByName[link.link];
+            if (!link.pid) {
+                link.broken = true;
+            }
+        });
     });
 
     return dict;
-  },
+},
 
-  /**
-   * The entry-point for converting Twine data into the Twison format.
-   */
-  convert: function() {
+/**
+ * The entry-point for converting Twine data into the Twison format.
+ */
+convert: function() {
     var storyData = document.getElementsByTagName("tw-storydata")[0];
     var json = JSON.stringify(Twison.convertStory(storyData), null, 2);
     document.getElementById("output").innerHTML = json;
-  }
+}
 }
 
 window.Twison = Twison;
